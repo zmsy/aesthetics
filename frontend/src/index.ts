be934b0a80ca211edd2094041d0499dd717f0dfe
Zach Morrissey <zachmor05@gmail.com>
2022-11-01 08:31:02 -0700

    Add better scaling

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -9,29 +9,26 @@ import { Network, Options } from "vis";
 
 const graphData = results as unknown as Array<Aesthetic>;
 
-type IndexedAesthetic = { idx: number } & Aesthetic;
+type IndexedAesthetic = { idx: number; numLinks: number } & Aesthetic;
 type GraphNode = { id: number; label: string; value: number };
 type GraphEdge = { from: number; to: number };
 
 // create a dataset where each aesthetic has an index.
 const graphDataLookup: Record<string, IndexedAesthetic> = Object.fromEntries(
-  graphData.map((g, idx) => [g.url, { ...g, idx }])
+  graphData.map((g, idx) => [g.url, { ...g, idx, numLinks: 0 }])
 );
 
 // now use that to create nodes and edges.
-const nodes: Array<GraphNode> = Object.values(graphDataLookup).map((a) => {
-  return {
-    id: a.idx,
-    label: a.name,
-    value: a.numPictures,
-  };
-});
-
 const edges: Array<GraphEdge> = Object.values(graphDataLookup)
   .flatMap((a) =>
     a.relatedAestheticUrls.map((r) => {
       const linkA = a.idx;
       const linkR = graphDataLookup[r.url]?.idx;
+
+      // increment number of links to that node
+      if (graphDataLookup[r.url]) {
+        graphDataLookup[r.url].numLinks += 1;
+      }
       return { from: linkA, to: linkR };
     })
   )
@@ -39,16 +36,34 @@ const edges: Array<GraphEdge> = Object.values(graphDataLookup)
   // to anything. happens sometimes with unindexed pages
   .filter((x) => x.to !== undefined);
 
-const data = { nodes, edges };
+const nodes: Array<GraphNode> = Object.values(graphDataLookup).map((a) => {
+  return {
+    id: a.idx,
+    label: a.name,
+    value:
+      a.numLinks +
+      a.relatedAestheticUrls.length +
+      Math.floor(a.numPictures / 2),
+  };
+});
+
+const data = { nodes, edges: [] };
 const options: Options = {
   nodes: {
     shape: "dot",
+    scaling: {
+      label: {
+        min: 5,
+        max: 82,
+      },
+    },
   },
   layout: {
     improvedLayout: false,
   },
 };
 
+console.log("Loading network graph...");
 var container = document.getElementById("network-graph-container");
 if (container) {
   new Network(container, data, options);
